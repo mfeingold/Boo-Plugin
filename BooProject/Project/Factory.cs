5ab6c729865ec5cca7a07b2a3e4d230dbd65606a
Kirill Skrygan <kskrygan@hill30.com>
2010-10-11 15:41:43 +0000

    new Setup with instructions

--- BooProject/Project/Factory.cs
+++ BooProject/Project/Factory.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.Project;
 using IOleServiceProvider = Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
+using Microsoft.Win32;
 
 namespace Hill30.BooProject.Project
 {
@@ -13,6 +14,8 @@ namespace Hill30.BooProject.Project
         public Factory(BooProjectPackage package)
             : base(package)
         {
+            if (Environment.GetEnvironmentVariable("BooBinPath", EnvironmentVariableTarget.User) == null)
+               BuildEngine.SetGlobalProperty("BooBinPath", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Microsoft\VisualStudio\10.0\Extensions\Hill30\BooLanguage\1.0");
             this.package = package;
         }
 
@@ -22,5 +25,6 @@ namespace Hill30.BooProject.Project
             result.SetSite((IOleServiceProvider)((IServiceProvider)this.package).GetService(typeof(IOleServiceProvider)));
             return result;
         }
+
     }
 }
