45fc63935f16b8d037e9d389170c3cf54b43ef00
Michael Feingold <mfeingold@hill30.com>
2011-04-03 09:58:31 -0500

    Booc40

---
+++ BoocNET40/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using booc;
+
+namespace BoocNET35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App.Main(args);
+        }
+    }
+}
